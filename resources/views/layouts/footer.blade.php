d4244dde80e2957e8be11dca664a5495bd95b536
Samirabizo <samirabizo24@gmail.com>
2025-06-21 16:46:08 +0100

    Mon second commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,54 @@
+<footer id="footer" class="footer dark-background">
+
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-12 footer-about">
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span class="sitename">GlobalTrade</span>
+          </a>
+          <p>Spécialistes des échanges commerciaux entre l'Afrique et l'Asie, nous facilitons vos opérations d'import/export avec le Nigeria, Togo, Chine, Ghana et Corée.</p>
+          <div class="social-links d-flex mt-4">
+            <a href=""><i class="bi bi-twitter-x"></i></a>
+            <a href=""><i class="bi bi-facebook"></i></a>
+            <a href=""><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Liens utiles</h4>
+          <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Conditions générales</a></li>
+            <li><a href="#">Politique de confidentialité</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Nos services</h4>
+          <ul>
+            <li><a href="#">Import depuis Asie</a></li>
+            <li><a href="#">Export vers Afrique</a></li>
+            <li><a href="#">Logistique internationale</a></li>
+            <li><a href="#">Douanes & Réglementations</a></li>
+            <li><a href="#">Approvisionnement</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+          <h4>Nous contacter</h4>
+          <p>AKEBE (APOSTROPHE)</p>
+          <p>Libreville, GABON</p>
+          <p class="mt-4"><strong>Téléphone:</strong> <span>+241 62 38 42 04</span></p>
+          <p><strong>Email:</strong> <span>contact@emirtravel.com</span></p>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">EMIR TRAVEL</strong> <span>Tous droits réservés</span></p>
+    </div>
+
+  </footer>
---
+++ resources/views/layouts/hero.blade.php
@@ -0,0 +1,95 @@
+<section id="hero" class="hero section dark-background">
+
+      <img src={{asset("assets/img/world-dotted-map.png")}} alt="" class="hero-bg" data-aos="fade-in">
+
+      <div class="container">
+        <div class="row gy-4 d-flex justify-content-between">
+          <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+            <h2 data-aos="fade-up">Votre partenaire en commerce international</h2>
+            <p data-aos="fade-up" data-aos-delay="100">Spécialistes des échanges commerciaux avec l'Afrique et l'Asie, nous facilitons vos opérations d'import/export vers et depuis le Nigeria, le Togo, la Chine, le Ghana et la Corée. Bénéficiez de notre expertise en logistique internationale et en conformité douanière.</p>
+
+            <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
+              <input type="text" class="form-control" placeholder="Votre produit ou pays d'intérêt">
+              <button type="submit" class="btn btn-primary">Rechercher</button>
+            </form>
+
+            <div class="row gy-4" data-aos="fade-up" data-aos-delay="300">
+
+              <div class="col-lg-3 col-6">
+                <div class="stats-item text-center w-100 h-100">
+                  <span data-purecounter-start="0" data-purecounter-end="156" data-purecounter-duration="0" class="purecounter">156</span>
+                  <p>Clients satisfaits</p>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-6">
+                <div class="stats-item text-center w-100 h-100">
+                  <span data-purecounter-start="0" data-purecounter-end="24" data-purecounter-duration="0" class="purecounter">24</span>
+                  <p>Pays desservis</p>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-6">
+                <div class="stats-item text-center w-100 h-100">
+                  <span data-purecounter-start="0" data-purecounter-end="982" data-purecounter-duration="0" class="purecounter">982</span>
+                  <p>Expéditions</p>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-6">
+                <div class="stats-item text-center w-100 h-100">
+                  <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="0" class="purecounter">2</span>
+                  <p>Ans d'expérience</p>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
+            <img src={{asset("assets/img/hero-img.svg" )}} class="img-fluid mb-3 mb-lg-0" alt="">
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+    <section id="featured-services" class="featured-services section">
+
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon flex-shrink-0"><i class="fa-solid fa-cart-flatbed"></i></div>
+            <div>
+              <h4 class="title">Import depuis l'Asie</h4>
+              <p class="description">Solutions clés en main pour importer des produits de Chine et Corée avec gestion des formalités douanières et assurance transport.</p>
+              <a href="#" class="readmore stretched-link"><span>En savoir plus</span><i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon flex-shrink-0"><i class="fa-solid fa-truck"></i></div>
+            <div>
+              <h4 class="title">Export vers l'Afrique</h4>
+              <p class="description">Expertise en exportation vers le Nigeria, Togo et Ghana avec connaissance approfondie des réglementations locales et des besoins du marché.</p>
+              <a href="#" class="readmore stretched-link"><span>En savoir plus</span><i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon flex-shrink-0"><i class="fa-solid fa-truck-ramp-box"></i></div>
+            <div>
+              <h4 class="title">Logistique internationale</h4>
+              <p class="description">Gestion complète du transport maritime, aérien et terrestre avec suivi en temps réel et optimisation des coûts.</p>
+              <a href="#" class="readmore stretched-link"><span>En savoir plus</span><i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
